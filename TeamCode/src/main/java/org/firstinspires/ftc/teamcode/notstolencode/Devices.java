e51eb58bceadfa1db557b8023c6c1e15325f3b0d
Rattlznake <147215321+Rattlznake@users.noreply.github.com>
2024-12-27 14:19:52 -0500

    Created and programmed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notstolencode/Devices.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.notstolencode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class Devices {
+    public DcMotorEx FLMotor;
+    public DcMotorEx FRMotor;
+    public DcMotorEx BLMotor;
+    public DcMotorEx BRMotor;
+
+    public IMU imu;
+
+    public Devices(HardwareMap hardwareMap) {
+        FLMotor = hardwareMap.get(DcMotorEx.class, "FLMotor");
+        FRMotor = hardwareMap.get(DcMotorEx.class, "FRMotor");
+        BLMotor = hardwareMap.get(DcMotorEx.class, "BLMotor");
+        BRMotor = hardwareMap.get(DcMotorEx.class, "BRMotor");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                )
+        ));
+
+        FLMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        FRMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        BLMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        BRMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+}
