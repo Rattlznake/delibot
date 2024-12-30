2f7f3795f4e4d3aa601b231455bfe5b67ba67fa3
Rattlznake <147215321+Rattlznake@users.noreply.github.com>
2024-12-30 15:24:17 -0500

    Added extra argument to FOD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DelibotTeleopButFOD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DelibotTeleopButFOD.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.notstolencode.Devices;
 
 import stolencode.FOD;
@@ -12,16 +15,29 @@ public class DelibotTeleopButFOD extends LinearOpMode {
     @Override public void runOpMode() {
         Devices dev = new Devices(hardwareMap);
         FOD fod = new FOD(dev);
+        YawPitchRollAngles robotYPR;
+        double joystickOrientation;
+
 
         waitForStart();
 
         while (opModeIsActive()) {
+            robotYPR = dev.imu.getRobotYawPitchRollAngles();
+
+            joystickOrientation = Math.atan2(gamepad1.right_stick_x,-gamepad1.left_stick_y);
+
+            telemetry.addData("robotYaw",robotYPR.getYaw(AngleUnit.RADIANS));
+
+            telemetry.addData("Joystick X",gamepad1.right_stick_x);
+            telemetry.addData("Joystick Y",-gamepad1.right_stick_y);
+            telemetry.addData("Joystick Radian Angle",joystickOrientation);
+            telemetry.update();
 
             if (gamepad1.y) {
                 fod.resetOrientation();
             }
 
-            fod.FODDrive(gamepad1.left_stick_y,gamepad1.left_stick_x, gamepad1.right_stick_x,1);
+            fod.FODDrive(-gamepad1.left_stick_y,gamepad1.left_stick_x, gamepad1.right_stick_x,-gamepad1.right_stick_y, 1);
         }
     }
 }
