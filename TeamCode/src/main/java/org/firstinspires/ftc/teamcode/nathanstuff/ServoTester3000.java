0836d60368da88a5e74fa6e2c608a3c854469449
005276 <147215321+Rattlznake@users.noreply.github.com>
2025-03-21 10:18:19 -0400

    change dev to devices

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/ServoTester3000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/ServoTester3000.java
@@ -6,16 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="Servo Tester 3000")
 public class ServoTester3000 extends LinearOpMode {
 
-    // A: Servo1 clockwise
-    // dpad down: Servo1 counter-clockwise
-    // B: Servo2 clockwise
-    // dpad right: Servo2 counter-clockwise
-    // X: Servo3 clockwise
-    // dpad left: Servo3 counter-clockwise
-    // Y: Servo4 clockwise
-    // dpad up: Servo4 counter-clockwise
     public void runOpMode() {
-        Devices dev = new Devices(hardwareMap);
+        Devices devices = new Devices(hardwareMap);
 
         double servoSpeed = 0.005;
 
@@ -23,28 +15,28 @@ public class ServoTester3000 extends LinearOpMode {
 
         while (opModeIsActive()) {
             if (gamepad1.a) { // servo 1 cw -- left claw
-                dev.leftClaw.setPosition(dev.leftClaw.getPosition()+servoSpeed);
+                devices.leftClaw.setPosition(devices.leftClaw.getPosition()+servoSpeed);
             }
             if (gamepad1.dpad_down) { // servo 1 ccw
-                dev.leftClaw.setPosition(dev.leftClaw.getPosition()-servoSpeed);
+                devices.leftClaw.setPosition(devices.leftClaw.getPosition()-servoSpeed);
             }
             if (gamepad1.b) { // servo 2 cw -- right claw
-                dev.rightClaw.setPosition(dev.rightClaw.getPosition()+servoSpeed);
+                devices.rightClaw.setPosition(devices.rightClaw.getPosition()+servoSpeed);
             }
-            if (gamepad1.dpad_left) { // servo 2 ccw
-                dev.rightClaw.setPosition(dev.rightClaw.getPosition()-servoSpeed);
+            if (gamepad1.dpad_right) { // servo 2 ccw
+                devices.rightClaw.setPosition(devices.rightClaw.getPosition()-servoSpeed);
             }
             if (gamepad1.x) { // servo 3 cw -- wrist roll
-                dev.wristRoll.setPosition(dev.wristRoll.getPosition()+servoSpeed);
+                devices.wristRoll.setPosition(devices.wristRoll.getPosition()+servoSpeed);
             }
-            if (gamepad1.dpad_right) { // servo 3 ccw
-                dev.wristRoll.setPosition(dev.wristRoll.getPosition()-servoSpeed);
+            if (gamepad1.dpad_left) { // servo 3 ccw
+                devices.wristRoll.setPosition(devices.wristRoll.getPosition()-servoSpeed);
             }
             if (gamepad1.y) { // servo 4 cw -- wrist pitch
-                dev.wristPitch.setPosition(dev.wristPitch.getPosition()+servoSpeed);
+                devices.wristPitch.setPosition(devices.wristPitch.getPosition()+servoSpeed);
             }
             if (gamepad1.dpad_up) { // servo 4 ccw
-                dev.wristPitch.setPosition(dev.wristPitch.getPosition()-servoSpeed);
+                devices.wristPitch.setPosition(devices.wristPitch.getPosition()-servoSpeed);
             }
         }
     }
