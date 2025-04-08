b73fa103f14fcc8c84fa09511fc87e65e2171efa
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-08 15:28:27 -0400

    make that there fod

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/BasicTeleopNothingNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/BasicTeleopNothingNew.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.nathanstuff;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 @TeleOp(name="BasicTeleOpNothingNew")
 public class BasicTeleopNothingNew extends LinearOpMode {
 
@@ -19,11 +21,22 @@ public class BasicTeleopNothingNew extends LinearOpMode {
             double leftStickY = -gamepad1.left_stick_y;
             double rightStickX = gamepad1.right_stick_x;
 
-            double denominator = Math.max(Math.abs(leftStickY) + Math.abs(leftStickX) + Math.abs(rightStickX), 1);
-            double flPower = (leftStickY + leftStickX + rightStickX) / denominator;
-            double blPower = (leftStickY - leftStickX + rightStickX) / denominator;
-            double frPower = (leftStickY - leftStickX - rightStickX) / denominator;
-            double brPower = (leftStickY + leftStickX - rightStickX) / denominator;
+            if (gamepad1.left_stick_button) {
+                devices.imu.resetYaw();
+            }
+
+            double botYaw = devices.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotatedX = leftStickX * Math.cos(-botYaw) - leftStickY * Math.sin(-botYaw);
+            double rotatedY = leftStickX * Math.sin(-botYaw) + leftStickY * Math.cos(-botYaw);
+
+            rotatedX *= 1.1;
+
+            double denominator = Math.max(Math.abs(rotatedY) + Math.abs(rotatedX) + Math.abs(rightStickX), 1);
+            double flPower = (rotatedY + rotatedX + rightStickX) / denominator;
+            double blPower = (rotatedY - rotatedX + rightStickX) / denominator;
+            double frPower = (rotatedY - rotatedX - rightStickX) / denominator;
+            double brPower = (rotatedY + rotatedX - rightStickX) / denominator;
 
             devices.flMotor.setPower(flPower);
             devices.frMotor.setPower(frPower);
