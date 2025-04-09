bec2ed3be23a7d8cf4c32f8055b14878b411047c
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-08 21:08:35 -0400

    write servo position changing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/PositionTester3000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/PositionTester3000.java
@@ -23,11 +23,25 @@ public class PositionTester3000 extends LinearOpMode {
             double rightStickX = gamepad1.right_stick_x;
             double rightStickY = -gamepad1.right_stick_y;
 
-            if (Math.abs(rightStickY) > 0.2) {
+            if (Math.abs(rightStickY) > 0.2) { // wrist pitch changing code
                 devices.wristPitch.setPosition(wristPitchPos+(rightStickY/100));
             }
-            if (Math.abs(rightStickX) > 0.2) {
+            if (Math.abs(rightStickX) > 0.2) { // wrist roll changing code
                 devices.wristRoll.setPosition(wristRollPos+(rightStickX/100));
+            }
+
+            if (gamepad1.right_bumper) { // right claw ccw
+                devices.rightClaw.setPosition(rightClawPos+0.01);
+            }
+            if (gamepad1.right_trigger > 0.2) { // right claw cw
+                devices.rightClaw.setPosition(rightClawPos-0.01);
+            }
+
+            if (gamepad1.left_bumper) { // left claw ccw
+                devices.leftClaw.setPosition(leftClawPos+0.01);
+            }
+            if (gamepad1.left_trigger > 0.2) { // left claw cw
+                devices.leftClaw.setPosition(leftClawPos-0.01);
             }
         }
     }
