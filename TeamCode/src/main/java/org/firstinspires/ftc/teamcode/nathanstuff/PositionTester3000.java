d456503f79a718d2325e1b6464de43faba4873e2
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-08 21:16:23 -0400

    write dcmotor position changing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/PositionTester3000.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/PositionTester3000.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.nathanstuff;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name="Position Tester 3000")
 public class PositionTester3000 extends LinearOpMode {
@@ -23,6 +25,11 @@ public class PositionTester3000 extends LinearOpMode {
             double rightStickX = gamepad1.right_stick_x;
             double rightStickY = -gamepad1.right_stick_y;
 
+            double leftStickY = -gamepad1.left_stick_y;
+
+            double servoPower = 0.01;
+            double dcMotorPower = 0.5;
+
             if (Math.abs(rightStickY) > 0.2) { // wrist pitch changing code
                 devices.wristPitch.setPosition(wristPitchPos+(rightStickY/100));
             }
@@ -31,18 +38,37 @@ public class PositionTester3000 extends LinearOpMode {
             }
 
             if (gamepad1.right_bumper) { // right claw ccw
-                devices.rightClaw.setPosition(rightClawPos+0.01);
+                devices.rightClaw.setPosition(rightClawPos+servoPower);
             }
             if (gamepad1.right_trigger > 0.2) { // right claw cw
-                devices.rightClaw.setPosition(rightClawPos-0.01);
+                devices.rightClaw.setPosition(rightClawPos-servoPower);
             }
 
             if (gamepad1.left_bumper) { // left claw ccw
-                devices.leftClaw.setPosition(leftClawPos+0.01);
+                devices.leftClaw.setPosition(leftClawPos+servoPower);
             }
             if (gamepad1.left_trigger > 0.2) { // left claw cw
-                devices.leftClaw.setPosition(leftClawPos-0.01);
+                devices.leftClaw.setPosition(leftClawPos-servoPower);
             }
+
+            devices.slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set to constant rotation
+            devices.wormGear.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // set to constant rotation
+
+            if (Math.abs(leftStickY) > 0.2) { // handle movement for worm gear (arm pitch)
+                devices.wormGear.setPower(dcMotorPower*leftStickY);
+            } else {
+                devices.wormGear.setPower(0);
+            }
+
+            if (gamepad1.dpad_up) { // handle movement for slides (arm extension)
+                devices.slides.setPower(dcMotorPower);
+            } else if (gamepad1.dpad_down) {
+                devices.slides.setPower(-dcMotorPower);
+            } else {
+                devices.slides.setPower(0);
+            }
+
+            // write telemetry code here
         }
     }
 }
