95cb2d3fa36283ae8a78fd86db28fa44df7c1770
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-09 15:24:58 -0400

    write class to store arm positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/ArmPosition.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.nathanstuff;
+
+public class ArmPosition {
+    double wristPitch;
+    double wristRoll;
+    int wormGear;
+    int slides;
+
+    public ArmPosition(double wristPitch, double wristRoll, int wormGear, int slides) {
+        this.wristPitch = wristPitch;
+        this.wristRoll = wristRoll;
+        this.wormGear = wormGear;
+        this.slides = slides;
+    }
+}
