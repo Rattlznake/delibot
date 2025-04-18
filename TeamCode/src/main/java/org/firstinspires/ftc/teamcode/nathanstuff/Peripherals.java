ad13d9d95213865830bbca330cbbc9936a726d44
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-17 20:50:31 -0400

    create and write Peripherals class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/Peripherals.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.nathanstuff;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class Peripherals {
+    public IMU imu;
+
+    public Peripherals(HardwareMap hardwareMap) {
+        this.imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(
+                new IMU.Parameters(
+                        new RevHubOrientationOnRobot(
+                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                                RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
+                        )
+                )
+        );
+    }
+}
