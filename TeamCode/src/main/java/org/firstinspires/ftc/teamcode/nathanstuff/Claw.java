04a3d5db5010b4b0e09b1e4f785e8fac92a076e6
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-04-17 20:50:14 -0400

    write Claw class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/Claw.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.nathanstuff;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Claw {
+    public Servo leftClaw;
+    public Servo rightClaw;
+    public Servo wristRollServo;
+    public Servo wristPitchServo;
+
+    public void setPincherGap(double pincherGap) {
+        // TODO: implement this
+    }
+
+    public void setWristRoll(double wristRoll) { // wrapper for setPosition for wristRollServo
+        wristRollServo.setPosition(wristRoll);
+    }
+
+    public void setWristPitch(double wristPitch) { // wrapper for setPosition for wristRollServo
+        wristPitchServo.setPosition(wristPitch);
+    }
+
+    public void setClawPosition(double wristRoll, double wristPitch) {
+        setWristRoll(wristRoll);
+        setWristPitch(wristPitch);
+    }
+
+    public Claw(HardwareMap hardwareMap) {
+        this.leftClaw = hardwareMap.get(Servo.class, "LeftClaw");
+        this.rightClaw = hardwareMap.get(Servo.class, "RightClaw");
+        this.wristRollServo = hardwareMap.get(Servo.class, "WristRoll");
+        this.wristPitchServo = hardwareMap.get(Servo.class, "WristPitch");
+    }
 }
