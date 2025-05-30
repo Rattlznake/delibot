95bde0f6d1bd8a0f29fba13d6563b467cc792f01
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-05-30 19:32:09 -0400

    Finish pincherGap function as well as add function (to be implemented) to run to a ClawPosition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/Claw.java
@@ -8,9 +8,21 @@ public class Claw {
     public Servo rightClaw;
     public Servo wristRollServo;
     public Servo wristPitchServo;
+    // TODO: Fill these in with appropriate values
+    public final double leftClawClosed = 0;
+    public final double rightClawClosed = 0;
 
     public void setPincherGap(double pincherGap) {
-        // TODO: implement this
+        leftClaw.setPosition(leftClawClosed+pincherGap);
+        rightClaw.setPosition(rightClawClosed-pincherGap);
+    }
+
+    public void open() {
+        this.setPincherGap(10); // TODO: Change this value to represent appropriate "open" claw distance
+    }
+
+    public void close() {
+        this.setPincherGap(0); // TODO: Change this value to represent appropriate "closed" claw distance
     }
 
     public void setWristRoll(double wristRoll) { // wrapper for setPosition for wristRollServo
@@ -24,6 +36,10 @@ public class Claw {
     public void setClawPosition(double wristRoll, double wristPitch) {
         setWristRoll(wristRoll);
         setWristPitch(wristPitch);
+    }
+
+    public void runToPosition(ClawPosition clawPosition) {
+        // TODO: Write code to run claw to a position
     }
 
     public Claw(HardwareMap hardwareMap) {
