2ec33bb956159caa79b6e52755012f29248cf997
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-05-30 19:31:11 -0400

    Finalize ArmPosition class to be used to store positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/ArmPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/ArmPosition.java
@@ -1,15 +1,43 @@
 package org.firstinspires.ftc.teamcode.nathanstuff;
 
 public class ArmPosition {
-    double wristPitch;
-    double wristRoll;
     int wormGear;
+    boolean useWormGear;
     int slides;
+    boolean useSlides;
 
-    public ArmPosition(double wristPitch, double wristRoll, int wormGear, int slides) {
-        this.wristPitch = wristPitch;
-        this.wristRoll = wristRoll;
+    // infinite parameters!!! don't use this.
+    public ArmPosition(int wormGear, boolean useWormGear, int slides, boolean useSlides) {
         this.wormGear = wormGear;
+        this.useWormGear = useWormGear;
         this.slides = slides;
+        this.useSlides = useSlides;
+    }
+
+    public ArmPosition() {
+        this.wormGear = 0;
+        this.useWormGear = false;
+        this.slides = 0;
+        this.useSlides = false;
+    }
+
+    // worm gear code
+    public void setWormGear(int wormGear) {
+        this.wormGear = wormGear;
+        this.useWormGear = true;
+    }
+
+    public void disableWormGear() {
+        this.useWormGear = false;
+    }
+
+    // slides code
+    public void setSlides(int slides) {
+        this.slides = slides;
+        this.useSlides = true;
+    }
+
+    public void disableSlides() {
+        this.useSlides = false;
     }
 }
