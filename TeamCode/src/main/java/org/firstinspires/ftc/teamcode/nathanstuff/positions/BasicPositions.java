25963791c8eed7a090ab962d2fa1e11284ac1515
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-06-02 06:35:20 -0400

    Created class to store basic positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nathanstuff/positions/BasicPositions.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.nathanstuff.positions;
+
+import org.firstinspires.ftc.teamcode.nathanstuff.ArmPosition;
+import org.firstinspires.ftc.teamcode.nathanstuff.ClawPosition;
+
+public class BasicPositions {
+    public ArmPosition armZeroPosition;
+
+    public ClawPosition clawZeroPosition;
+
+    public BasicPositions() {
+        armZeroPosition = new ArmPosition();
+        armZeroPosition.setWormGear(0);
+        armZeroPosition.setSlides(0);
+
+        clawZeroPosition = new ClawPosition() // TODO: Fill this in with appropriate zero position
+        clawZeroPosition.setWristPitch(0);
+        clawZeroPosition.setWristRoll(0);
+    }
+}
